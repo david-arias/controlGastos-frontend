e5586a2f2698d54f39cb69eec464571612a61ccf
david-arias <devius123@gmail.com>
2026-01-16 17:44:57 -0500

    added login

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,97 +1,208 @@
 import "./login.css";
 
-import React, { useState } from "react";
-import { BiUser, BiLock, BiShow } from "react-icons/bi";
+import { useState } from "react";
+import { BiUser, BiLock, BiShow, BiSolidShow, BiMailSend } from "react-icons/bi";
+import { ToastContainer, toast } from 'react-toastify';
+
+
+import api from '../../api/axios';
 
 const Login = () => {
-    let [isActive, setIsActive] = useState(true);
-    const [formData, setFormData] = useState({ username: "", password: "" });
+    let [isLogin, setIsLogin] = useState(true);
+    let [isPassVisisble, setIsPassVisisble] = useState(true);
+
+    const [formData, setFormData] = useState({ username: "", password: "", email: "", currency: "" });
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        console.log(formData);
+
+        if (isLogin) {
+            const loginData = { "username": formData.username, "password": formData.password }
+            console.log(loginData);
+
+            // const notifyError = (mssgErrorTtl: string, mssgErrorBody: string) => toast.error((<div>
+            //     <h4>{mssgErrorTtl}</h4>
+            //     <p>{mssgErrorBody}</p>
+            // </div>), { theme: "dark" });
+            // try {
+            //     const res = await api.post('/auth/login', formData);
+            //     console.log(res.data);
+            //     // localStorage.setItem('token', res.data.token);
+            //     // localStorage.setItem('user', JSON.stringify(res.data.user));
+            //     //navigate('/dashboard'); // Redirigir al dashboard tras éxito
+            // } catch (err: any) {
+            //     notifyError(err.response?.data?.title || 'Error al iniciar sesión', err.response?.data?.text || '');
+            // }
+        } else {
+            const loginData = { "username": formData.username, "password": formData.password, "email": formData.email }
+            console.log(loginData);
+        }
+
+
+    }
 
     return (
         <>
-            <div className="topInfo">top info</div>
+            <div className="topInfo">
+                <div className="topMssg">
+                    <h2>¡Hola de nuevo! 🤗</h2>
+                    <p>Ingresa a tu cuenta o crea una nueva para organizar tus finanzas.</p>
+                </div>
+            </div>
             <div className="bottomForm">
                 <div className="topTab">
                     <div className="box">
-                        <div className={`pillBox ${isActive ? "active" : ""}`}>
+                        <div className={`pillBox ${isLogin ? "active" : ""}`}>
                             &nbsp;
                         </div>
                         <button
                             className="item"
-                            onClick={() => setIsActive((isActive = true))}
+                            onClick={() => { setIsLogin((isLogin = true)); }}
                         >
-                            <p>Login</p>
+                            <p>Iniciar sesión</p>
                         </button>
                         <button
                             className="item"
-                            onClick={() => setIsActive((isActive = false))}
+                            onClick={() => { setIsLogin((isLogin = false)); }}
                         >
-                            <p>Register</p>
+                            <p>Registrarse</p>
                         </button>
                     </div>
                 </div>
 
                 <div className="formBox">
-                    {isActive ? (
+                    {isLogin ? (
                         <div className="loginForm">
-                            <form action="">
+                            <form onSubmit={handleSubmit}>
                                 <div className="itemField">
                                     <div className="fieldBox">
-                                        <div className="icon">
-                                            <BiUser />
-                                        </div>
-                                        <div className="textField">
-                                            <label htmlFor="" className="lbl">
-                                                Username
-                                            </label>
-                                            <input
-                                                type="text"
-                                                onChange={(e) =>
-                                                    setFormData({
-                                                        ...formData,
-                                                        username:
-                                                            e.target.value,
-                                                    })
-                                                }
-                                                required
-                                            />
+                                        <div className="fields">
+                                            <div className="icon">
+                                                <BiUser />
+                                            </div>
+                                            <div className="textField">
+                                                <input id="field-user"
+                                                    type="text"
+                                                    placeholder="&nbsp;"
+                                                    onChange={(e) =>
+                                                        setFormData({
+                                                            ...formData,
+                                                            username:
+                                                                e.target.value,
+                                                        })
+                                                    }
+                                                    required
+                                                />
+                                                <label htmlFor="field-user" className="lbl">
+                                                    Usuario
+                                                </label>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="itemField">
                                     <div className="fieldBox">
-                                        <div className="icon">
-                                            <BiLock />
+                                        <div className="fields">
+                                            <div className="icon">
+                                                <BiLock />
+                                            </div>
+                                            <div className="textField">
+
+                                                <input id="field-pass"
+                                                    type={isPassVisisble ? "password" : "text"}
+                                                    placeholder="&nbsp;"
+                                                    onChange={(e) =>
+                                                        setFormData({
+                                                            ...formData,
+                                                            password:
+                                                                e.target.value,
+                                                        })
+                                                    }
+                                                    required
+                                                />
+                                                <label htmlFor="field-pass" className="lbl">
+                                                    Contraseña
+                                                </label>
+                                            </div>
                                         </div>
-                                        <div className="textField">
-                                            <label htmlFor="" className="lbl">
-                                                Password
-                                            </label>
-                                            <input
-                                                type="password"
-                                                onChange={(e) =>
-                                                    setFormData({
-                                                        ...formData,
-                                                        password:
-                                                            e.target.value,
-                                                    })
-                                                }
-                                                required
-                                            />
-                                        </div>
-                                        <button>
-                                            <BiShow />
-                                        </button>
+                                        {isPassVisisble ? (
+                                            <button type="button" onClick={() => setIsPassVisisble((isPassVisisble = false))} >
+                                                <BiShow />
+                                            </button>
+                                        ) : (
+                                            <button type="button" onClick={() => setIsPassVisisble((isPassVisisble = true))} >
+                                                <BiSolidShow />
+                                            </button>
+                                        )}
                                     </div>
                                 </div>
-                                <button className="summitField">Sign in</button>
+                                <button className="summitField" type="submit">Iniciar sesión</button>
                             </form>
                         </div>
                     ) : (
-                        <p>register</p>
+                        <div className="RegisterForm">
+                            <form onSubmit={handleSubmit}>
+                                <div className="itemField">
+                                    <div className="fieldBox">
+                                        <div className="fields">
+                                            <div className="icon">
+                                                <BiUser />
+                                            </div>
+                                            <div className="textField">
+                                                <input id="fieldReg-user"
+                                                    type="text"
+                                                    placeholder="&nbsp;"
+                                                    onChange={(e) =>
+                                                        setFormData({
+                                                            ...formData,
+                                                            username:
+                                                                e.target.value,
+                                                        })
+                                                    }
+                                                    required
+                                                />
+                                                <label htmlFor="fieldReg-user" className="lbl">
+                                                    Usuario
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="itemField">
+                                    <div className="fieldBox">
+                                        <div className="fields">
+                                            <div className="icon">
+                                                <BiMailSend />
+                                            </div>
+                                            <div className="textField">
+                                                <input id="fieldReg-email"
+                                                    type="email"
+                                                    placeholder="&nbsp;"
+                                                    onChange={(e) =>
+                                                        setFormData({
+                                                            ...formData,
+                                                            email:
+                                                                e.target.value,
+                                                        })
+                                                    }
+                                                    required
+                                                />
+                                                <label htmlFor="fieldReg-email" className="lbl">
+                                                    Correo electrónico
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button className="summitField" type="submit">Registrarte</button>
+                            </form>
+                        </div>
                     )}
                 </div>
             </div>
+
+            <ToastContainer />
         </>
     );
 };
